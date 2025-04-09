243480fe7f14d942953af7b4015f5fa144a43604
bhargavi12458 <bhargavi12458@gmail.com>
2025-04-09 10:52:38 +0530

    Add files via upload

---
+++ exp2.c
@@ -0,0 +1,24 @@
+>>> a=10
+>>> b=20
+>>> a=int(input("enter first number"))
+enter first number10
+>>> b=int(input("enter second number"))
+enter second number20
+>>> print(a+b)
+30
+>>> print(a-b)
+-10
+>>> print(a*b)
+200
+>>> print(a/b)
+0.5
+>>> print(a%b)
+10
+>>> print(a//b)
+0
+>>> print(a**b)
+100000000000000000000
+>>> a=eval(input("enter the first number"))
+enter the first number10
+>>> b=eval(input("enter the second number"))
+enter the second number20
