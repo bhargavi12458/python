285ca99e4c2b28cb5e7fdfda638b4fd2a87fdcbf
bhargavi12458 <bhargavi12458@gmail.com>
2025-04-09 10:54:57 +0530

    Add files via upload

---
+++ list.c
@@ -0,0 +1,8 @@
+my list=input("enter a list of number sepsrated by space:")
+my list=list(ma(int,my list,space list))
+sum=0
+for num in my list:
+    sum t=wm
+    print("the sum of the number is:",sum)
+    
+    
