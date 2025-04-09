61bcf7c2e68cf9643055135953223177daca0d89
bhargavi12458 <bhargavi12458@gmail.com>
2025-04-09 10:54:19 +0530

    Add files via upload

---
+++ format.c
@@ -0,0 +1,24 @@
+num=int(input("enter a number:"))
+if num==0:
+print("zero")
+exit num==1:
+    print("one")
+    exit num==2:
+        print("two")
+        exit num==3:
+            print("three")
+            exit num==4:
+                print("four")
+                exit num==5:
+                print("five")
+                exit num==6:
+                print("six")
+                exit num==7:
+                print("seven")
+                exit num==8:
+                print("eight")
+                exit num==9:
+                print("nine")
+                else:
+                    print("number out range:")
+                    
