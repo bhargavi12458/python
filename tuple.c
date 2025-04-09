1b14d7320c06286fe1ac369733e9288a11bcad39
bhargavi12458 <bhargavi12458@gmail.com>
2025-04-09 11:01:03 +0530

    Add files via upload

---
+++ tuple.c
@@ -0,0 +1,15 @@
+>>> t=10,20,30
+>>> print(type(t))
+<class 'tuple'>
+>>> print(t)
+(10, 20, 30)
+>>> t=()
+>>> print(type(t))
+<class 'tuple'>
+>>> print(t)
+()
+>>> t=(10, )
+>>> print(type(t))
+<class 'tuple'>
+>>> print(t)
+(10,)
