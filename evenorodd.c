3fad809d52119f8a765aa9f5c00e60eba4eaf00a
bhargavi12458 <bhargavi12458@gmail.com>
2025-04-09 10:56:12 +0530

    Add files via upload

---
+++ evenorodd.c
@@ -0,0 +1,7 @@
+num=int(input(enter a number:"))
+              it num%2==0:
+              print(num",is even:)
+              else:
+                  print(num",is odd:)
+                        
+                        
