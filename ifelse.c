2012934f5c9ae49260aa85b908d8b5b115888a73
bhargavi12458 <bhargavi12458@gmail.com>
2025-04-09 10:54:36 +0530

    Add files via upload

---
+++ ifelse.c
@@ -0,0 +1,6 @@
+num=int(input("enter a number:"))
+if num>=1 and num<=100:
+    print("the number","number","in between number")
+    else:
+        print("the number","is num","is not between number")
+        
