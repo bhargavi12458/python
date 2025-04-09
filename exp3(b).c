bc2950e26f73cee74986dfab78dd062795332c0f
bhargavi12458 <bhargavi12458@gmail.com>
2025-04-09 10:53:22 +0530

    Add files via upload

---
+++ exp3(b).c
@@ -0,0 +1,15 @@
+>>> a=int(input("enter first number:"))
+enter first number:10
+>>> b=int(input("enter second number:"))
+enter second number:20
+>>> a=a+b
+>>> b=a-b
+>>> a=a-b
+>>> print("after swapping of two numbers are:")
+after swapping of two numbers are:
+>>> print(".....")
+.....
+>>> print("first value is:",a)
+first value is: 20
+>>> print("second value is:",b)
+second value is: 10
