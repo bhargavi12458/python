74e774bb3aa8fd78dd5866b18187a5d9ff3f6330
bhargavi12458 <bhargavi12458@gmail.com>
2025-06-18 12:46:07 +0530

    Add files via upload

---
+++ 10.1.python.py
@@ -0,0 +1,5 @@
+try:
+    x=10/0
+    except exception as e:
+        print("an error occured:",str(e))
+        
---
+++ 10.3.python.py
@@ -0,0 +1,10 @@
+try:
+    num+int(input("enter a number:"))
+    result=10/num
+    except value error:
+        print("error:invalid input!please enter a valid number")
+        except zero division error:
+            print("error:division by zero!")
+            else:
+                print("result:",result)
+                
