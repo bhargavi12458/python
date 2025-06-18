d8de77832e0263fb63b5d28a82158507e9ca4886
bhargavi12458 <bhargavi12458@gmail.com>
2025-06-18 12:40:43 +0530

    Add files via upload

---
+++ 1.4.python.py
@@ -0,0 +1,10 @@
+a=int(input("enter first number:"))
+b=int(input("enter second number:"))
+temp=a
+a=b
+b=temp
+print("after swapping of two numbers are:")
+print("___")
+print("first value is:",a)
+print("second value is:",b)
+
---
+++ 2.5.python.py
@@ -0,0 +1,8 @@
+n=int(input("enter the value of n:"))
+count=0
+num=5
+while count n:
+    print(num)
+    num t=5
+    count t=1
+    
